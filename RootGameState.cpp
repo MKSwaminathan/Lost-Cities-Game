9e23d503c8278ff7873dad29fc71dd5e9c7bc66c
MKSwaminathan <meenakshi.swaminathan@utexas.edu>
2017-09-15 23:53:24 -0500

    initial for rootgamestate

---
+++ RootGameState.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <globals.h>
+#include <vector>
+
+#define NUM_COLORS 5
+
+
+class RootGameState{
+
+  public:
+  // player columns -- 2D color x card
+  static std::vector< std::vector<Card> > player_columns;
+  static std::vector< std::vector<Card> > ai_columns;
+   
+  // scores
+  // discard pile
+  // deck size  
+  // player hand
+  // AI hand
+     
+  RootGameState(); // constr.
+// newgame/reset
+// update root state 
+// get current score
+  
+
+};
+
+//Member fxns 
