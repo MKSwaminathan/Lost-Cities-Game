09a86d63f0256399e18faa1d411c94a63a293718
Erick Shepherd <erickshepherd@utexas.edu>
2017-09-16 00:16:56 -0500

    Update LostCitiesMain.cpp

--- LostCitiesMain.cpp
+++ LostCitiesMain.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 
+void Set_Up_List() {
+
+	int x, y;
+	
+	for (x = 0; x < 5; x++) {
+	
+		for (y = 0; y < 12; y++) {
+			
+			All_Possible[12 * x + y].color = x;
+					
+			if(y > 8) {
+						
+				All_Possible[12 * x + y].value = 1;
+			}
+
+			else {
+			
+				All_Possible[12 * x + y].value = y + 2;
+			}
+		}
+	}
+}
+
 int main() {
-  std::cout << "Hi there." << std::endl;
+  Set_Up_List();
   return 0;
 
 }
