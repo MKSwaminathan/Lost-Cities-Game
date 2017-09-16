3c03465d46f434331188714b362f33a92b7dbe5b
MKSwaminathan <meenakshi.swaminathan@utexas.edu>
2017-09-16 15:51:14 -0500

    members to deck

--- Deck.cpp
+++ Deck.cpp
@@ -1,7 +1,33 @@
+#include <iostream>
 #include "Deck.h"
 
+// c++ inits
+std::vector< std::vector<bool> > Deck::possible_cards_mask(0, std::vector<bool>(0));
+std::vector<Card> Deck::current_deck(0);
 int Deck::size;
 
-Deck::Deck(){
+void Deck::initPossibleCardsMask() {
+	for (int i = 0; i < NUM_COLORS - 1; i++){
+		for (int j = 0; j <  NUM_CARDS_PER_COLOR - 1; j++) {
+			Deck::possible_cards_mask[i][j] = true;
+		}
+	}
+}
+
+void Deck::shuffle() {
+
+}
+
+Card Deck::draw(){
+	Deck::size --;
+	Card drawn = Deck::current_deck.back();
+	Deck::current_deck.pop_back();
+	return drawn;
+}
+
+Deck::Deck() {
+	Deck::current_deck.resize(NUM_TOTAL_CARDS);
+	Deck::possible_cards_mask.resize(NUM_COLORS, std::vector<bool>(NUM_CARDS_PER_COLOR));
+	initPossibleCardsMask();
 	Deck::size = NUM_TOTAL_CARDS;
 }
--- Deck.h
+++ Deck.h
@@ -1,13 +1,20 @@
 #ifndef DECK_H
 #define DECK_H
 
+#include <vector>
 #include "Globals.h"
 
 class Deck {
 	public:
+	static std::vector< std::vector<bool> > possible_cards_mask;
+	static std::vector<Card> current_deck;
+
 	static int size;
-	
+			
 	Deck();
+	static void initPossibleCardsMask();
+	static void shuffle();
+	static Card draw();
 };
 
 #endif // DECK_H
